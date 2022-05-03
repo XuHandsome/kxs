a9d0c193d2f9a9de0a23841b1c749b20a06bee0e
handsomexu <chinaops666@gmail.com>
2022-05-03 16:05:56 +0800

    调整获取api方式，支持每个函数自定义client使用的类

--- libs/cluster.py
+++ libs/cluster.py
@@ -1,6 +1,5 @@
-from kubernetes.client import api_client
-from kubernetes.client.api import core_v1_api
-from kubernetes import client
+from kubernetes import client, watch
+from kubernetes.client.rest import ApiException
 
 
 class Kubernetes(object):
@@ -8,28 +7,29 @@ class Kubernetes(object):
         self.api_host = api_host
         self.token = token
 
-    def get_api(self):
+    def get_configuration(self):
         configuration = client.Configuration()
         configuration.api_key = {"authorization": "Bearer " + self.token}
         configuration.host = self.api_host
         # 跳过ssl验证
         configuration.verify_ssl = False
 
-        client1 = api_client.ApiClient(configuration=configuration)
-        api = core_v1_api.CoreV1Api(client1)
-        return api
+        return configuration
 
     def health_check(self):
-        api = self.get_api()
-        system_pods = []
-        try:
-            for pod in api.list_namespaced_pod('kube-system').items:
-                system_pods.append(pod.metadata.name)
-        except Exception as e:
-            print(e)
-            return False
 
-        if system_pods:
-            return True
-        else:
-            return False
+        configuration = self.get_configuration()
+        with client.ApiClient(configuration) as api_client:
+            api_instance = client.CoreV1Api(api_client)
+            system_pods = []
+            try:
+                for pod in api_instance.list_namespaced_pod('kube-system').items:
+                    system_pods.append(pod.metadata.name)
+            except ApiException as e:
+                print("Exception when calling CoreV1Api->list_namespaced_pod: %s\n" % e)
+                return False
+
+            if system_pods:
+                return True
+            else:
+                return False
